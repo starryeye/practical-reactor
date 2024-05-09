d8a3ebec385e36f5187b1b0939d6388ac7000bc7
starryeye <see2818@naver.com>
2024-05-09 23:41:22 +0900

    [starryeye, 2024.05.09] - c2_TransformingSequence, maybe
    내용
    - solved

--- exercises/src/test/java/c2_TransformingSequence.java
+++ exercises/src/test/java/c2_TransformingSequence.java
@@ -89,7 +89,7 @@ public class c2_TransformingSequence extends TransformingSequenceBase {
     @Test
     public void maybe() {
         Mono<String> result = maybe_service()
-                //todo: change this line only
+                .defaultIfEmpty("no results")
                 ;
 
         StepVerifier.create(result)

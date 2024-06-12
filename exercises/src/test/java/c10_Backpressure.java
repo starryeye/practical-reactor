ca0f3dd8c561e6b3a032511401df2f7b2ddece72
starryeye <see2818@naver.com>
2024-06-12 20:42:59 +0900

    [starryeye, 2024.06.12] - c10_Backpressure, request_and_demand
    내용
    - solved

--- exercises/src/test/java/c10_Backpressure.java
+++ exercises/src/test/java/c10_Backpressure.java
@@ -47,7 +47,7 @@ public class c10_Backpressure extends BackpressureBase {
     public void request_and_demand() {
         CopyOnWriteArrayList<Long> requests = new CopyOnWriteArrayList<>();
         Flux<String> messageStream = messageStream1()
-                //todo: change this line only
+                .doOnRequest(requests::add) // request 이벤트를 hooking 하여, 요청된 값을 requests 에 넣는다.
                 ;
 
         StepVerifier.create(messageStream, StepVerifierOptions.create().initialRequest(0))

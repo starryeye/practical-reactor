757780a4eec25f1f392864044c1250702d6b40dd
starryeye <see2818@naver.com>
2024-06-18 20:35:01 +0900

    [starryeye, 2024.06.18] - c12_Broadcasting, history_lesson
    내용
    - solved

--- exercises/src/test/java/c12_Broadcasting.java
+++ exercises/src/test/java/c12_Broadcasting.java
@@ -44,7 +44,9 @@ public class c12_Broadcasting extends BroadcastingBase {
          *      모든 subscriber 가 해제되면 sequence 를 중지하고, 다시 지정된 수의 subscriber 가 connect 되면 기존 sequence 를 이어서 시작한다. (이어서 시작)
          * - refCount() : 지정된 수의 subscriber 가 connect 될 때 sequence 를 시작하며,
          *      모든 subscriber 가 해제되면 sequence 를 중지하고, 다시 지정된 수의 subscriber 가 connect 되면 새로운 sequence 를 시작한다. (다시 처음부터 시작)
-         * - share() : publish().refCount(1)의 단축형
+         *
+         * 참고
+         * share() : publish().refCount(1)의 단축형
          *      최소 한 명의 subscriber 가 connect 되면 sequence 를 시작하고, 모든 subscriber 가 해제되면 sequence 를 중지한다.
          *
          *
@@ -77,6 +79,7 @@ public class c12_Broadcasting extends BroadcastingBase {
      */
     @Test
     public void hot_vs_cold() {
+
         Flux<String> updates = systemUpdates()
                 .publish()
                 .autoConnect(1)
@@ -100,9 +103,21 @@ public class c12_Broadcasting extends BroadcastingBase {
      */
     @Test
     public void history_lesson() {
+
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
+                .cache()
                 ;
+        /**
+         * 참고
+         * systemUpdates()
+         *  그대로 놔두어도 정답 처리 된다. 기본적으로 Flux 는 cold publisher 이기 때문이다.
+         * systemUpdates().publish().refCount(1) (= systemUpdates().share())
+         *  cold 에서 hot 으로 변경하였지만, refCount(1) 으로 인해 cold 처럼 동작된다.
+         * systemUpdates().cache()
+         *  cache 가 중간에서 지금까지 발행(첫번째 subscriber 에게 발행)된 item 들을 캐싱하고 있다가
+         *  새로운 subscriber 에게 빠르게 전달 후 다시 원본 publisher 에 접근하여 이후 item 들을 발행 받는다.
+         *  -> 두번째 subscriber 에게는 5 개의 item 중.. 3개는 delay 없이 한번에 발행되는 모습을 확인 가능
+         */
 
         //subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
